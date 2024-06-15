d9487b591a24ab6c0871f77662832d854929c815
KhronoLabyrinth <96112679+KhronoLabyrinth@users.noreply.github.com>
2024-06-15 02:21:14 +0100

    Delete constants.py (renamed)

--- constants.py
+++
@@ -1,4 +0,0 @@
-alpha = 3 # distance exponent of impact scaling down
-gridsize = 100 # width and height of the grid
-iterations = 100
-impact_range = 15 # max x/y coordinate difference for other individuals to impact an individual
